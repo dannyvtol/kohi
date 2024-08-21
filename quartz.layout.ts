632f0f2b04b5495caa0579eb1c54f5fda5480e66
Danny van Tol <danny.van.tol@outlook.com>
2024-08-21 06:43:21 +0200

    fix: Make sure the explorer osorts the same way on the listing page
    Use a single configured Explorer component instead of multiple for different page layouts.
    
    Prioritize files over folders and sort folders by physical name instead of the display name.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -2,6 +2,36 @@ import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 import { FileNode } from "./quartz/components/ExplorerNode";
 
+const Explorer = Component.Explorer({
+  sortFn: (fileNodeA: FileNode, fileNodeB: FileNode) => {
+    // Prioritize files before folders in listing
+    if (fileNodeA.children.length === 0 && fileNodeB.children.length > 0) {
+      return -1;
+    }
+
+    if (fileNodeA.children.length > 0 && fileNodeB.children.length === 0) {
+      return 1;
+    }
+
+    if (fileNodeA.file !== null && fileNodeB.file !== null) {
+      const frontmatterWeightA: number = (fileNodeA.file.frontmatter?.weight ?? 999) as number;
+      const frontmatterWeightB = (fileNodeB.file.frontmatter?.weight ?? 999) as number;
+    
+      if (frontmatterWeightA < frontmatterWeightB) {
+        return -1;
+      }
+    
+      return 1;
+    }
+
+    // Sort folders by physical name instead of display name
+    return fileNodeA.name.localeCompare(fileNodeB.name, undefined, {
+      numeric: true,
+      sensitivity: "base",
+    });
+  }
+})
+
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
@@ -30,25 +60,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   left: [
     Component.DesktopOnly(
-      Component.Explorer({
-        sortFn: (fileNodeA: FileNode, fileNodeB: FileNode) => {
-          if (fileNodeA.file !== null && fileNodeB.file !== null) {
-            const frontmatterWeightA: number = (fileNodeA.file.frontmatter?.weight ?? 999) as number;
-            const frontmatterWeightB = (fileNodeB.file.frontmatter?.weight ?? 999) as number;
-          
-            if (frontmatterWeightA < frontmatterWeightB) {
-              return -1;
-            }
-          
-            return 1;
-          }
-
-          return fileNodeA.displayName.localeCompare(fileNodeB.displayName, undefined, {
-            numeric: true,
-            sensitivity: "base",
-          });
-        }
-      })
+      Explorer
     ),
   ],
   right: [
@@ -61,7 +73,9 @@ export const defaultContentPageLayout: PageLayout = {
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(
+      Explorer
+    ),
   ],
   right: [],
 }
